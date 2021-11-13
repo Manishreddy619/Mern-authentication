2ae9d48b900c10fa830680a00219ad0f523a4d94
Manish Elaganti <65448066+Manishreddy619@users.noreply.github.com>
2021-11-13 18:27:52 +0100

    add files

--- config/db.js
+++ config/db.js
@@ -1,14 +1,14 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.DATABASE_CONNECTION, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: true,
-  });
+	await mongoose.connect(process.env.MONGO_CONNECTION, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+		useFindAndModify: true,
+	});
 
-  console.log("MongoDB Connected");
+	console.log('MongoDB Connected');
 };
 
 module.exports = connectDB;
